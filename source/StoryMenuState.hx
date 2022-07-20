cfa66eb427bac99c6b2023d09b43e161e537c3fc
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-07-20 10:16:31 -0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -228,7 +228,11 @@ class StoryMenuState extends MusicBeatState
         
 
 		changeWeek();
-
+		
+                #if android
+	        addVirtualPad(LEFT_FULL, A_B);
+                #end
+			
 		super.create();
 	}
 
